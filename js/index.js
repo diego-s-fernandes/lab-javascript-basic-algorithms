900875deb642ba8f7cafc1b6f566f818e2827664
Diego Fernandes <superdiegao@gmail.com>
2022-10-19 15:24:44 -0300

    exercício finalizado

--- js/index.js
+++ js/index.js
@@ -33,3 +33,44 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+/* 
+3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+
+3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+
+3.3 Depending on the lexicographic order of the strings, print:
+- The driver's name goes first.
+- Yo, the navigator goes first definitely.
+- What?! You both have the same name?
+*/
+let newDriversName = ""; // esta é a variável que será preenchida com o loop
+for(let i=0; i < hacker1.length; i++) {
+  newDriversName += hacker1[i].toUpperCase() + " ";
+}
+console.log(newDriversName.trim());
+// ao invés de comaçar por i = 0, vou começar por 1 igual ao tamanho do nome
+// para haver conflitos nos nomes das variávels setei a nova variável acrescentada do número 2
+
+let newNavigatorsName2 = "";
+for(let i = hacker2.length - 1; i >= 0; i--) {
+  newNavigatorsName2 += hacker2[i];
+}
+console.log(newNavigatorsName2);
+
+// Neste exercício eu fiquei em dúvida e procurei ajuda na internet. Há mais de um jeito de fazer a regra, escrevi abaixo a que fez mais sentido para meu entendimento, ou sej, a regra em que eu eu entendi a lógica do que está sendo feito.
+
+hacker1 = hacker1.toLowerCase();
+hacker2 = hacker2.toLowerCase();
+
+if (hacker1 > hacker2) {
+  console.log("The driver's name goes first.")
+}
+else if (hacker1 < hacker2) {
+  console.log("Yo, the navigator goes first definitely.")
+}
+else {
+  console.log("What?! You both have the same name?")
+}
+
+
+// vou fazer o git do exercício obrigatório e caso eu termine os exercícios bonus, envio ainda hoje
